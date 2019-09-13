186de95bceb69f3735de0702b3bf8a1893b377e4
taras.tomishch <ttomishch@email.com>
2019-09-13 15:10:43 +0200

    Initial

---
+++ BoschRexroth.Root/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace BoschRexroth.Root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var capture = new VideoCapture(@"VID_20190831_120949.mp4");
+
+            var frame = new Mat();
+            while (capture.Read(frame))
+            {
+                if (frame.Width <= 0 || frame.Height <= 0)
+                {
+                    break;
+                }
+
+                Cv2.ImShow("frame", frame);
+
+                var delay = (int)(1000 / capture.Fps);
+                var key = Cv2.WaitKey(delay);
+                if (key == 27)
+                    break;
+            }
+
+        }
+    }
+}
